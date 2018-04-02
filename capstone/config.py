3fe9f7540fb015cf53aafd4d97f1dd688ce5ef52
Jeon-Young-Hoon <31722979+Jeon-Young-Hoon@users.noreply.github.com>
2018-04-02 11:29:52 +0900

    Delete config.py

--- capstone/config.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-
-start_time = datetime.datetime.now()
-start_time_str=now.strftime('%Y_%m_%d_%H_%M_%S')
-
-img_data_path='./img_data/'+nowstr
-video_data_path='./video_data/'
-feature_data_path='./feature_data/'
